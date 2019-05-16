b061b7ebf15201075d21a5dce8d438632bf0ad06
davidragel21 <49564645+davidragel21@users.noreply.github.com>
2019-05-16 14:11:37 +0100

    Add files via upload

---
+++ src/gmusic/src/gmusic/DBase.java
@@ -0,0 +1,62 @@
+package gmusic;
+
+import java.sql.*;
+import java.sql.DriverManager;
+/**
+ * @desc A singleton database access class for MySQL
+ * @author Ramindu
+ */
+public final class DBase {
+    public Connection conn;
+    private Statement statement;
+    public static DBase db;
+    DBase() {
+        String url= "jdbc:mysql://localhost:3306/";
+        String dbName = "project1";
+        String driver = "com.mysql.jdbc.Driver";
+        String userName = "ns";
+        String password = "root";
+        try {
+            Class.forName(driver).newInstance();
+            this.conn = (Connection)DriverManager.getConnection(url+dbName,userName,password);
+        }
+        catch (Exception sqle) {
+            sqle.printStackTrace();
+        }
+    }
+    /**
+     *
+     * @return DBase Database connection object
+     */
+    public static synchronized DBase getDbCon() {
+        if ( db == null ) {
+            db = new DBase();
+        }
+        return db;
+ 
+    }
+    /**
+     *
+     * @param query String The query to be executed
+     * @return a ResultSet object containing the results or null if not available
+     * @throws SQLException
+     */
+    public ResultSet query(String query) throws SQLException{
+        statement = db.conn.createStatement();
+        ResultSet res = statement.executeQuery(query);
+        return res;
+    }
+    /**
+     * @desc Method to insert data to a table
+     * @param insertQuery String The Insert query
+     * @return boolean
+     * @throws SQLException
+     */
+    public int insert(String insertQuery) throws SQLException {
+        statement = db.conn.createStatement();
+        int result = statement.executeUpdate(insertQuery);
+        return result;
+ 
+    }
+ 
+}
